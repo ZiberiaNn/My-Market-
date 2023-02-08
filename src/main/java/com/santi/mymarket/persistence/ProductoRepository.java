1bc03892e1f0e856aa0958b5804a872e4420d942
Santiago Jaramillo <jaramilloocamposantiago@gmail.com>
2023-02-08 14:57:36 -0500

    Fix: Typos and inaccesible fields

--- src/main/java/com/santi/mymarket/persistence/ProductoRepository.java
+++ src/main/java/com/santi/mymarket/persistence/ProductoRepository.java
@@ -1,19 +1,45 @@
 package com.santi.mymarket.persistence;
 
+import com.santi.mymarket.domain.Product;
+import com.santi.mymarket.domain.repository.ProductRepository;
 import com.santi.mymarket.persistence.crud.ProductoCrudRepository;
 import com.santi.mymarket.persistence.entity.Producto;
+import com.santi.mymarket.persistence.mapper.ProductMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class ProductoRepository {
+public class ProductoRepository implements ProductRepository {
     private ProductoCrudRepository productoCrudRepository;
+    private ProductMapper mapper;
 
-    public List<Producto> getAll() {
-        return (List<Producto>) productoCrudRepository.findAll();
+    public List<Product> getAll() {
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
+        return mapper.toProducts(productos);
     }
+
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<List<Product>> getScarseProducts(int quantity) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Product save(Product product) {
+        return null;
+    }
+
     public List<Producto> getByCategoria(int idCategoria){
         return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
     }
